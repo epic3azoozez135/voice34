60abe2e0a6d01b0ee4dac89b759ebb757782be0a
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-12-12 01:00:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,65 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const fs = require('fs')
+const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
-var prefix = "#"
+const prefix = "#";
+ 
+const devs = ["516583452699656192"]
+ 
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**- # تـم تـحويـل إلـى وضعيـهة ، Online . :lollipop: ${argresult}**`)
+  } else
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**- # تـم تـحويـل إلـى وضعيـهة ، WATCHING . :lollipop: ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`** - # تـم تـحويـل إلـى وضعيـهة ، LISTENING . :lollipop: ${argresult}**`)
+  } else    
+    if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}**`)
+  return message.reply("**- # تـم تحويـل أسـم الـبوت ألـى .. :lollipop:**");
+  } else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : - # تـم تحويـل آلصـورة إلـى .. :lollipop:`);
+        } else    
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Mrx-Dev");
+      message.channel.sendMessage(`** - # تـم تـحويـل إلـى وضعيـهة ، Streaming . :lollipop:  ${argresult}**`)
+  }
+    if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`:warning:️ ** restarting by ${message.author.username}**`);
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(` Bot restarting...Maaaz `);
+        console.log("===============================================\n\n");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`Bot Successfully Restarted`);
+    }
+ 
+  });
+client.login(process.env.BOT_TOKEN);
 
+
+
+
+
+
+//كود يدخل البوت الروم الصوتي
 client.on('message', msg => {
 
     if (msg.content == '#join') {
@@ -14,8 +72,5 @@ client.on('message', msg => {
 }
 })
 client.on('ready', () => {
-    client.channels.get("517598518534209537").join();
+    client.channels.get("517597861060280321").join();
     });
-
-
-client.login(process.env.TOKEN);
