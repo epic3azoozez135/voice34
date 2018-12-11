65190017a03cbd6dda9b0ed30d1b0677fc7ac63a
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-12-12 00:45:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "#"
+
+client.on('message', msg => {
+
+    if (msg.content == '#join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("id Room").join();
+    });
+
+
+client.login(process.env.TOKEN);
